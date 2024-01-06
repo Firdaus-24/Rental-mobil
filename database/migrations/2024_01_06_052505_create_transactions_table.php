a79bb480ebb3e1e892becb7c0a352c13fe089223
Firdaus-24 <firdaus.rvb@gmail.com>
2024-01-06 15:15:38 +0700

    update

---
+++ database/migrations/2024_01_06_052505_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cars_id');
+            $table->foreignId('users_id');
+            $table->dateTime('tgl_mulai');
+            $table->dateTime('tgl_selesai');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
